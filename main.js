cc3814871fed46764bfda0213501108913d6f6b9
Mhretab Kidane <mhretab@users.noreply.github.com>
2020-01-22 15:35:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,41 +2,30 @@ const core = require('@actions/core');
 const { GitHub, context } = require('@actions/github');
 
 async function main() {
-    let prNumber, currentBranch, latestSha;
+    let prNumber, currentBranch;
+    let latestSha = context.payload.after;
+
     if (context.payload.pull_request) {
         prNumber = context.payload.pull_request.number;
-        latestSha = context.payload.after;
         currentBranch = context.payload.pull_request.head.ref;
     } else {
-        prNumber = 1;
-        latestSha = '';
-        currentBranch = '';
+        prNumber = '';
+        currentBranch = context.payload.ref.split('refs/heads/')[0];
     }
-    
+
+    if(!prNumber && branches.includes(currentBranch)) {
+        core.setOutput("deployID", "")
+    } else {
+        core.setOutput("deployID", `/${prNumber || latestSha}`);
+    }
+
     console.log("prNumber", prNumber);
     console.log("latestSha", latestSha);
     console.log("branch", currentBranch);
-    
-    console.log("payload", context.payload);
-    const token = core.getInput('github-token', { required: true });
-    console.log(`Hello ${token}!`);
-    const sha = core.getInput('sha');
-    console.log(`Hello sha ${token}!`);
-    const client = new GitHub(token, {});
-    const result = await client.repos.listPullRequestsAssociatedWithCommit({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        commit_sha: sha || context.sha,
-    });
 
-    const pr = result.data.length > 0 && result.data[0];
-    console.log("Context", context.repo);
-    console.log("pr", pr);
-    console.log("branch", context.repo.branch);
-    core.setOutput('pr', pr && pr.number || prNumber);
-    core.setOutput('number', pr && pr.number || prNumber);
-    core.setOutput('title', pr && pr.title || '');
-    core.setOutput('body', pr && pr.body || '');
+    core.setOutput('pr', prNumber);
+    core.setOutput('branch', currentBranch);
+    core.setOutput('latestSha', latestSha);
 }
 
 main().catch(err => core.setFailed(err.message));
